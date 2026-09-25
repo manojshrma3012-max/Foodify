ced13c0809a2327f6e5913b938e75c71ab60e2ce
Manoj Sharma <manojshrma3012@gmail.com>
2026-09-25 23:19:32 +0530

    Setting up realtime server using socket.io

---
+++ services/socketserver/src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+const app = express()
+app.use(cors())
+dotenv.config()
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Realtime server running on ${process.env}`)
+})
